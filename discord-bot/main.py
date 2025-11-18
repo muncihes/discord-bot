84130807250b42fe48f360879fa555d416fb15da
muncihes <boraninc15@gmail.com>
2025-11-18 03:55:14 +0300

    Add files via upload

---
+++ discord-bot/main.py
@@ -0,0 +1,16 @@
+import os
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Bot aktif: {bot.user}")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("Pong!")
+
+bot.run(os.getenv("TOKEN"))
